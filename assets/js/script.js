749cc48434eabd5f281aaeb156057955c28842a4
Anjali Rajendran <84264801+AnjaliRaj015@users.noreply.github.com>
2023-04-11 22:49:21 +0530

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,42 +168,41 @@ nav_button.addEventListener('click', function () {
 // // To fade out the element
 // fadeElement(".hide1", "out");
 
-const delay = 3000; //ms
 
-const slides = document.querySelector(".slides");
-const slidesCount = slides.childElementCount;
-const maxLeft = (slidesCount - 1) * 10 * -1;
-
-let current = 0;
-
-function changeSlide(next = true) {
-  if (next) {
-    current += current > maxLeft ? -100 : current * -1;
-  } else {
-    current = current < 0 ? current + 100 : maxLeft;
-  }
-
-  slides.style.left = current + "%";
-}
-
-let autoChange = setInterval(changeSlide, delay);
-const restart = function() {
-  clearInterval(autoChange);
-  autoChange = setInterval(changeSlide, delay);
-};
-
-// Controls
-document.querySelector(".next-slide").addEventListener("click", function() {
-  changeSlide();
-  restart();
-});
-
-document.querySelector(".prev-slide").addEventListener("click", function() {
-  changeSlide(false);
-  restart();
-});
 
 setInterval(function() {
   var element = document.querySelector('#title1');
   element.classList.toggle('glitch');
 }, 3000);
+
+
+  //<![CDATA[
+    let toggles = document.getElementsByClassName("toggle");
+    let contentDiv = document.getElementsByClassName("content");
+    let icons = document.getElementsByClassName("icon");
+
+    for (let i = 0; i < toggles.length; i++) {
+        toggles[i].addEventListener("click", () => {
+            if (parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight) {
+                contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
+                toggles[i].style.color = "#0084e9";
+                icons[i].classList.remove("fa-plus");
+                icons[i].classList.add("fa-minus");
+            } else {
+                contentDiv[i].style.height = "0px";
+                toggles[i].style.color = "#111130";
+                icons[i].classList.remove("fa-minus");
+                icons[i].classList.add("fa-plus");
+            }
+
+            for (let j = 0; j < contentDiv.length; j++) {
+                if (j !== i) {
+                    contentDiv[j].style.height = 0;
+                    toggles[j].style.color = "#111130";
+                    icons[j].classList.remove("fa-minus");
+                    icons[j].classList.add("fa-plus");
+                }
+            }
+        });
+    }
+//]]>
