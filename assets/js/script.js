bcd302e1cd627bfce2163abd8a2dc97ff8a6050f
Anjali Rajendran <84264801+AnjaliRaj015@users.noreply.github.com>
2023-04-12 21:25:03 +0530

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,12 +185,12 @@ setInterval(function() {
         toggles[i].addEventListener("click", () => {
             if (parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight) {
                 contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
-                toggles[i].style.color = "#0084e9";
+                toggles[i].style.color = "#01feff";
                 icons[i].classList.remove("fa-plus");
                 icons[i].classList.add("fa-minus");
             } else {
                 contentDiv[i].style.height = "0px";
-                toggles[i].style.color = "#111130";
+                toggles[i].style.color = "#01feff";
                 icons[i].classList.remove("fa-minus");
                 icons[i].classList.add("fa-plus");
             }
@@ -198,7 +198,7 @@ setInterval(function() {
             for (let j = 0; j < contentDiv.length; j++) {
                 if (j !== i) {
                     contentDiv[j].style.height = 0;
-                    toggles[j].style.color = "#111130";
+                    toggles[j].style.color = "white";
                     icons[j].classList.remove("fa-minus");
                     icons[j].classList.add("fa-plus");
                 }
